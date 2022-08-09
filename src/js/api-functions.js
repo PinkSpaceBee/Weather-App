10b1052af6bfcaa53b8cef49e9d2bf44ddb53711
name <minaary@mailfence.com>
2022-08-09 21:52:50 +0300

    add geocode func

---
+++ src/js/api-functions.js
@@ -0,0 +1,32 @@
+// my key 921d13d1f0511dece8b22ba91fc75172
+
+// get latitude and longitude
+export async function geocode() {
+    
+    try {
+        const response = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=kyiv&appid=921d13d1f0511dece8b22ba91fc75172');
+        
+        // since fetch doesn't throw an error for https errors I wrote a conditional to catch 4xx and 5xx responses
+        if (response.ok) {
+            const data = await response.json();
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            return {lat, lon};
+
+        } else {
+            throw new Error (`oops. ${response.status}`);
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getWeatherData() {
+
+    let lat = await geocode();
+    let lon = await geocode();
+
+    lat = lat.lat;
+    lon = lon.lon;
+}
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+'use strict';
+import * as weatherApi from './api-functions';
+
+weatherApi.geocode();
+weatherApi.getWeatherData();
