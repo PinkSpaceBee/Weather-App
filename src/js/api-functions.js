6b00902798207f61d0c61002f6415a1a9e262ced
name <minaary@mailfence.com>
2022-08-10 23:17:14 +0300

    tweak geocode function

--- src/js/api-functions.js
+++ src/js/api-functions.js
@@ -1,11 +1,16 @@
 // my key 921d13d1f0511dece8b22ba91fc75172
 
+export function getCityName(form) {
+const city = form.value;
+return city;
+}
+
 // get latitude and longitude
-export async function geocode() {
-    
+export async function geocode(city) {
+
     try {
-        const response = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=kyiv&appid=921d13d1f0511dece8b22ba91fc75172');
-        
+        const response = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=921d13d1f0511dece8b22ba91fc75172`);
+
         // since fetch doesn't throw an error for https errors I wrote a conditional to catch 4xx and 5xx responses
         if (response.ok) {
             const data = await response.json();
@@ -16,17 +21,20 @@ export async function geocode() {
         } else {
             throw new Error (`oops. ${response.status}`);
         }
-        
+
     } catch (error) {
         console.log(error)
     }
 }
 
-export async function getWeatherData() {
-
-    let lat = await geocode();
-    let lon = await geocode();
-
-    lat = lat.lat;
-    lon = lon.lon;
+export async function getWeatherData(lat, lon) {
+    console.log(`test ${lat} ${lon}`)
 }
+
+// let lat = await geocode();
+// let lon = await geocode();
+
+// lat = lat.lat;
+// lon = lon.lon;
+
+// console.log(lat, lon);
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 'use strict';
 import * as weatherApi from './api-functions';
 
-weatherApi.geocode();
-weatherApi.getWeatherData();
+const searchBtn = document.querySelector('.search-bar > button');
+
+searchBtn.addEventListener('click', () => {
+    // fetch data from the form
+    const city = weatherApi.getCityName(document.querySelector('input'));
+
+    weatherApi.geocode(city)
+        .then((response) => {
+            console.log(response);
+            weatherApi.getWeatherData(response.lat, response.lon);
+        })
+});
+
+
+//weatherApi.getWeatherData();
