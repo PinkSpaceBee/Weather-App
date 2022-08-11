8c6daeb912cbe80dd00ce2b9149c0b450a69cd17
name <minaary@mailfence.com>
2022-08-11 10:45:02 +0300

    write func to get current weather

--- src/js/api-functions.js
+++ src/js/api-functions.js
@@ -1,15 +1,15 @@
-// my key 921d13d1f0511dece8b22ba91fc75172
+const KEY = '921d13d1f0511dece8b22ba91fc75172';
 
 export function getCityName(form) {
 const city = form.value;
 return city;
 }
 
-// get latitude and longitude
+// get latitude and longitude by location name
 export async function geocode(city) {
 
     try {
-        const response = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=921d13d1f0511dece8b22ba91fc75172`);
+        const response = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${KEY}`, {mode: 'cors'});
 
         // since fetch doesn't throw an error for https errors I wrote a conditional to catch 4xx and 5xx responses
         if (response.ok) {
@@ -27,14 +27,11 @@ export async function geocode(city) {
     }
 }
 
+// get current weather data using geo coordinates
 export async function getWeatherData(lat, lon) {
-    console.log(`test ${lat} ${lon}`)
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`, {mode: 'cors'});
+
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
 }
-
-// let lat = await geocode();
-// let lon = await geocode();
-
-// lat = lat.lat;
-// lon = lon.lon;
-
-// console.log(lat, lon);
