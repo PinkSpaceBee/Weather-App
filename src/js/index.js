8fd141e37bc53f3f369eb25c1c21c1fb4ff452e9
name <minaary@mailfence.com>
2022-08-18 00:10:17 +0300

    messing with time

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import * as weatherApi from './api-functions';
 import * as domFunc from './dom-functions';
 
-let city = 'london';
+let city = 'london'; 
 let weather;
 
 async function getWeatherData() {
@@ -29,6 +29,38 @@ async function displayWeather() {
 
     //create section to display current weather data
     domFunc.displayCurrWeather(document.querySelector('.weather-data'), weather.current.temp, weather.current.weather[0].description, weather.current.feels_like, weather.current.wind_speed, weather.current.humidity);
+
+    // ok apparently I can use daily.temp[time_of_day], nice
+    // but, if I use it, I won't be able to get the weather description for every hour, only temperature
+    // eh I'd rather go with hourly in this case
+
+    // returns every 3rd hour 
+    const hourly = weather.hourly
+    .splice(24)
+    //.filter(function(value, index, arr) {
+    //    return index % 3 === 0;
+    //})
+    .map(elem => {
+        //const timestamp = elem.dt + weather.timezone_offset;
+        const timestamp = elem.dt + weather.timezone_offset;
+        const ms = timestamp * 1000;
+        const time = new Date(ms).toLocaleString();
+        return time;
+    })
+;
+
+    console.log(hourly);
+    // to get utc
+    const now = new Date();
+    const nowUtc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+
+    const ts = nowUtc.getTime();
+    // for some reason returns -1 hour, like 17:07 instead of 18:07
+    // huh, now it returns correct time, wtf. I'll check tomorrow, I gonna hit the bed now
+    const offset = ts + weather.timezone_offset * 1000;
+
+    console.log(ts);
+    console.log(new Date(offset).toLocaleString());
 }
 
 displayWeather()
